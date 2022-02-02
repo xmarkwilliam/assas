ce3d3f7547eab9316fb8cbf945e372d271e53d4a
Toronking <xmarkwilliam@yahoo.com>
2022-02-02 15:16:04 +0400

    Update progress_chart.js

--- progress_chart.js
+++ progress_chart.js
@@ -2,17 +2,18 @@ const gtx = document.getElementById('Progress').getContext('2d');
 const Progress = new Chart(gtx, {
     type: 'bar',
     data: {
-        labels: ['ABU DHABI', 'DUBAI','AL AIN', 'SHARJAH', 'RAS AL KHAIMAH', 'AJMAN', 'UMM AL QUWAIN', 'FUJAIRAH'],
+        labels: ['ABU DHABI', 'DUBAI', 'SHARJAH', 'RAS AL KHAIMAH', 'AJMAN', 'UMM AL QUWAIN', 'FUJAIRAH'],
         datasets: [{
             label: 'PROJECTS ON SEVEN EMIRATES', 
-            data: [80, 55, 99, 87, 45, 65,69,50,69],
+            data: [80, 55, 99, 87, 45, 65,69],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 12)',
+                'rgba(75, 192, 192, )',
+                'rgba(153, 102, 255, 12)',
+                'rgba(255, 159, 64, 12)'
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
